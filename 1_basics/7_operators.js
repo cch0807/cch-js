bb692ae5478017cd4e44aff3aea3d203d9a8ee8e
cch0807 <ckdgus11021@gmail.com>
2023-06-15 00:09:49 +0900

    feat: hoisting

---
+++ 1_basics/7_operators.js
@@ -0,0 +1,23 @@
+/**
+ * Operators
+ * 
+ * 연산자
+ */
+
+/**
+ * 산술 연산자
+ * 
+ * 1) 덧셈
+ * 2) 뺄셈
+ * 3) 곱셈
+ * 4) 나눗셈
+ * 5) 나머지
+ */
+console.log(10 + 10);
+console.log(10 - 10);
+console.log(10 * 10);
+console.log(10 / 10);
+console.log(10 % 10);
+console.log(10 % 3);
+
+console.log('------------');
