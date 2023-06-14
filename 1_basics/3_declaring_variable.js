a3245463eff7ce8b60a796b235e9883d33f23464
cch0807 <ckdgus11021@gmail.com>
2023-06-14 22:26:08 +0900

    feat: basics

---
+++ 1_basics/3_declaring_variable.js
@@ -0,0 +1,45 @@
+/*
+Variable 선언하기
+1) var - 더 이상 쓰지 않는다.
+2) let - 
+3) const - 
+*/
+var name = 'cch';
+
+console.log(name);
+
+var age = 27;
+
+console.log(age);
+
+let test = '테스트';
+console.log(test);
+
+/*
+let과 var로 선언하면
+값을 추후 변경 할 수 있다.
+*/
+
+test = 'test2';
+console.log(test);
+
+const newJeans = '뉴진스';
+console.log(newJeans)
+
+// newJeans = 'jeansnew';
+
+/**
+ * 선언과 할당
+ * 
+ * 1) 변수를 선언하는 것.
+ * 2) 할당
+ */
+
+var name = 'cch';
+console.log(name);
+
+let temp;
+console.log(temp)
+
+// const는 불가능 
+// const temp2; 
