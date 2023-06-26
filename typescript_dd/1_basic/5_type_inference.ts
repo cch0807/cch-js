7f6075199480975a9436d52a0a21780e1d786d7a
ch.choi <ch.choi@aizen.co>
2023-06-26 17:47:23 +0900

    feat: enum & type inference

---
+++ typescript_dd/1_basic/5_type_inference.ts
@@ -0,0 +1,58 @@
+/**
+ * Type Inference
+ *
+ * 타입 추론
+ */
+
+let stringType = 'string';
+let booleanType = true;
+let numberType = 30;
+
+booleanType = false;
+
+// booleanType = 'false'; // error
+
+const constStringType = 'const string';
+const constBooleanType = true;
+
+let yuJin = {
+    name: '안유진',
+    age: 2003,
+};
+
+const yuJin2 = {
+    name: '안유진',
+    age: 2003,
+};
+
+yuJin2.name = 'yujin';
+console.log(yuJin2);
+
+const yuJin3 = {
+    name: '안유진' as const,
+    age: 2003 as const,
+};
+
+// yuJin3.name = 'yujin'; // error
+console.log(yuJin3.name);
+console.log(yuJin2.name);
+
+/**
+ * Array
+ */
+let numbers = [1, 2, 3, 4, 5];
+let numbersAndString = [1, 2, 3, '4', '5', '6'];
+
+// numbers.push('6'); // error
+numbers.push(6);
+const number = numbers[0];
+const nos = numbersAndString[0];
+const nos2 = numbersAndString[6];
+
+// tuple
+const twoNumbers = [1, 3] as const;
+
+// twoNumbers[0] = 10;
+// twoNumbers.push(100);
+const first = twoNumbers[0];
+// const first2 = twoNumbers[3];
