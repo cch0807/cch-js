5f737303d930627485797e5c2c4b92b1445aca16
cch0807 <ckdgus11021@gmail.com>
2023-08-26 00:37:36 +0900

    feat: object key value mapping & class intro

---
+++ typescript/9_class/1_defining_class.ts
@@ -0,0 +1,27 @@
+/**
+ * 클래스 선언하기
+ */
+
+class SampleClass {}
+
+class Game {
+    name: string;
+    country: string;
+    download: number;
+
+    constructor(name: string, country: string, download: number) {
+        this.name = name;
+        this.country = country;
+        this.download = download;
+    }
+
+    introduce() {
+        return `${this.name}은 ${this.country} 에서 제작된 ${this.download} 개의 다운로드를 달성한 게임이다!`;
+    }
+}
+
+const starcraft = new Game('StarCraft', 'USA', 10000000);
+
+const retVal = starcraft.introduce();
+
+// console.log(retVal);
